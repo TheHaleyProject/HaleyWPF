cd43e3a2132ec5b7e3677432a94fbb378f28deb5
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2021-05-14 16:22:27 +0400

    few more updates

--- _TESTING/WPF.Test/App.xaml.cs
+++ _TESTING/WPF.Test/App.xaml.cs
@@ -6,6 +6,8 @@ using Haley.MVVM;
 using Haley.Utils;
 using System.Windows;
 using WPF.Test.Controls;
+using Haley.WPF.BaseControls;
+using System.Windows.Media;
 
 namespace WPF.Test
 {
@@ -16,7 +18,7 @@ namespace WPF.Test
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            ContainerRegistrations();
+            //ContainerRegistrations();
             //ExpanderTest _wndw2sd = new ExpanderTest();
 
             //MainWindow _wndwo = new MainWindow();
@@ -40,9 +42,37 @@ namespace WPF.Test
             //RibbonTest wnddgtest = new RibbonTest();
             //wnddgtest.ShowDialog();
 
-            NewFlexiMenu flexiNewMenuTest = new NewFlexiMenu();
-            flexiNewMenuTest.ShowDialog();
+            //NewFlexiMenu flexiNewMenuTest = new NewFlexiMenu();
+            //flexiNewMenuTest.ShowDialog();
+            notificationTest();
+        }
 
+
+        private void notificationTest()
+        {
+            var mw = Application.Current.MainWindow;
+            if (mw == null)
+            {
+                //First invoke.
+                mw = new Window(); //Dummy initiation
+            }
+            IDialogService _ds = ContainerStore.Singleton.DI.Resolve<IDialogService>();
+            //_ds.ShowDialog("Test", "hello world");
+            //_ds.ShowDialog("What", "this is to hide the icon", hideIcon: true);
+
+            //var _baseAccent = (SolidColorBrush)new BrushConverter().ConvertFromString("Purple");
+
+            //_ds.ChangeAccentColors(_baseAccent);
+            //_ds.ShowDialog("MyGoodness", "Warning, you are going to die at 90", NotificationIcon.Warning);
+            //var _data = _ds.ShowDialog("Confirm", "Do you know that you are an idiot", mode: DialogMode.Confirmation);
+            //var _data2 = _ds.ShowDialog("Name", "please write your name ", NotificationIcon.Error, DialogMode.GetInput);
+
+            _ds.SendToast("Processing Error 1", "Error while doing this");
+            //_ds.SendToast("Processing Error2", "Error while doing this");
+            //_ds.SendToast("Processing Error 3", "Error while doing this");
+            //_ds.SendToast("Processing Error 3", "Error while doing this");
+            //_ds.SendToast("Processing Erro 5 r", "Error while doing this");
+            //_ds.SendToast("Processing Error 6", "Error while doing this");
         }
 
         private void ContainerRegistrations()
